15b2dea76491b4098afd8742b36a333dd1af2c10
Kazuki Hayashi <hayashi.kazuki.55a@gmail.com>
2025-12-16 17:45:49 +0900

    Update __init__.py

--- dktshell/__init__.py
+++ dktshell/__init__.py
@@ -1,7 +1,7 @@
 from .core import DKTAnalysis
 
 __copyright__    = 'Copyright (C) 2025 Kazuki Hayashi and Romain Mesnil'
-__version__      = '0.1.1'
+__version__      = '0.1.2'
 __license__      = 'MIT license'
 __author__       = 'Kazuki Hayashi and Romain Mesnil'
 __author_email__ = 'hayashi.kazuki@archi.kyoto-u.ac.jp'
